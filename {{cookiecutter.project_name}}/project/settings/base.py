91e16c7a560dfe75cf9d84f6152ade2faff6d3ae
Maurizo Branca <maurizio@unbit.is>
2014-12-15 18:39:53 +0100

    Fixed ALLOWED_HOSTS
    The ALLOWED_HOSTS should contain the production_domain.

--- {{cookiecutter.project_name}}/project/settings/base.py
+++ {{cookiecutter.project_name}}/project/settings/base.py
@@ -192,8 +192,8 @@ MANAGERS = ADMINS
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = [
-    '{{cookiecutter.current_domain}}',
-    'www.{{cookiecutter.current_domain}}',
+    '{{cookiecutter.production_domain}}',
+    'www.{{cookiecutter.production_domain}}',
 ]
 
 # Local time zone for this installation. Choices can be found here:
